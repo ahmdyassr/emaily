803b9d7a452bdc403c468e6598f1ce1745f70c62
Ahmed Yasser <ahmdyassr@gmail.com>
2022-09-19 05:19:35 +0200

    Add a simple request

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const express = require('express')
 const app = express()
 
+app.get('/', (req, res) => {
+	res.send('Hello World!')
+})
+
 app.listen(process.env.PORT, () => {
 	console.log(`Server is listening on port ${process.env.PORT}`)
 })
